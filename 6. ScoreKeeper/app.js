4f1e74cb0036dcd39203a4152d4f81ff1080e3b9
singhoc <70388970+singhoc@users.noreply.github.com>
2022-07-10 14:31:33 +0530

    Score Keeper Refactored using Objects

--- 6. ScoreKeeper/app.js
+++ 6. ScoreKeeper/app.js
@@ -1,42 +1,40 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
-const p1Display = document.querySelector("#p1Score");
-const p2Display = document.querySelector("#p2Score");
+const p1 = {
+    score: 0,
+    button: document.querySelector("#p1Button"),
+    display: document.querySelector("#p1Score")
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector("#p2Button"),
+    display: document.querySelector("#p2Score")
+}
+
 const reset = document.querySelector("#reset");
 const scoreSelect = document.querySelector("#playTo");
 
-
-let p1Score = 0;
-let p2Score = 0;
 let winOn = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function () {
+function scoreKeeper(player, opponent) {
     if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winOn) {
+        player.score += 1;
+        if (player.score === winOn) {
             isGameOver = true;
-            p1Display.classList.add("has-text-success");
-            p2Display.classList.add("has-text-danger");
-            p1Button.disabled = true;
-            p2Button.disabled = true;
+            player.display.classList.add("has-text-success");
+            opponent.display.classList.add("has-text-danger");
+            player.button.disabled = true;
+            opponent.button.disabled = true;
         }
-        p1Display.textContent = p1Score;
+        player.display.textContent = player.score;
     }
+}
+
+p1.button.addEventListener('click', function () {
+    scoreKeeper(p1, p2);
 })
 
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winOn) {
-            isGameOver = true;
-            p2Display.classList.add("has-text-success");
-            p1Display.classList.add("has-text-danger");
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-        }
-        p2Display.textContent = p2Score;
-    }
+p2.button.addEventListener('click', function () {
+    scoreKeeper(p2, p1);
 })
 
 scoreSelect.addEventListener('change', function () {
@@ -46,13 +44,12 @@ scoreSelect.addEventListener('change', function () {
 reset.addEventListener('click', re)
 
 function re() {
-    p1Score = 0;
-    p2Score = 0;
     isGameOver = false;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('has-text-success', 'has-text-danger')
-    p2Display.classList.remove('has-text-success', 'has-text-danger')
-    p1Button.disabled = false;
-    p2Button.disabled = false;
+    for (p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+
 }
