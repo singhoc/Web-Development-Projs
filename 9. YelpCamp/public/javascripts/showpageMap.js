5cdd1859441df65af4f9785db3e7d7232c1b3e5b
singhoc <70388970+singhoc@users.noreply.github.com>
2024-05-29 16:13:15 +0530

    Cleaned up YelpCamp

--- 9. YelpCamp/public/javascripts/showpageMap.js
+++ 9. YelpCamp/public/javascripts/showpageMap.js
@@ -5,6 +5,7 @@ const map = new mapboxgl.Map({
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+map.addControl(new mapboxgl.NavigationControl())
 
 // Create a new marker.
 const marker = new mapboxgl.Marker()
